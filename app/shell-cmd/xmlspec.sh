45b3dc111b4bf7e9c1f3f05301febbf0809ae00f
Phil Fearon <pgfearo@gmail.com>
2014-12-07 11:58:27 +0000

    directory tidying

--- app/shell-cmd/xmlspec.sh
+++ app/shell-cmd/xmlspec.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# syntax-highlight and color source code files for:
+# syntax-highlight and color source code file for:
 # xslt xquery xpath xsd schematron xproc
 #
-# requires Saxon XSLT 2.0 processor (replace SAXONJAR below)
+# requires Saxon XSLT 2.0 processor from http://saxonica.com (replace SAXONJAR below)
 
 SAXONJAR="/Users/philipfearon/Applications/SaxonHE9-6-0-2J/saxon9he.jar"
 # entry point is XMLSpectrum's highlight-file xsl stylesheet:
 XMLSPECTRUM=$(dirname $0)/../xsl/highlight-file.xsl
 
+# this script resolves relative paths in the sourcepath based on the current directory
+# that the script is called from - not necessarily the script path
+
 # first arg must be the source file path
+# run script with no args to get full usage (returned by highllight-file.xsl)
 
 if [[ $# -eq 0 ]]
     then
