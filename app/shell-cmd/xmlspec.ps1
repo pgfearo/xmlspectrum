7df19dfbb0602d5707be6a40595f17a690ef0a1b
Phil Fearon <pgfearo@gmail.com>
2014-12-07 11:29:44 +0000

    tidying up directory structure

--- app/shell-cmd/xmlspec.ps1
+++
@@ -1,7 +0,0 @@
-$shome = get-childitem -path env:SAXON_HOME
-$SAXONJAR="$shome/saxon9he.jar"
-# entry point is XMLSpectrum's highlight-file xsl stylesheet:
-$XMLSPECTRUM="$PSScriptRoot\..\xsl/highlight-file.xsl"
-echo jar path: $SAXONJAR
-iex "java -jar $SAXONJAR"
-
