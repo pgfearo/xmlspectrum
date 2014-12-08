2453053b37600af1e7134cce537abd8270857075
pgfearo <pgfearo@gmail.com>
2014-12-08 16:44:45 +0000

    new xslprint.bat

---
+++ app/shell-cmd/xslprint.bat
@@ -0,0 +1,29 @@
+@echo off
+REM script for calling xmlspectrum with command line such as: xmlspec sample.xml auto-trim=yes indent=2
+REM loop is required to combine name/value pairs with an '=' as args are split as they are passed to CMD
+set source=%~f1
+echo source: %source%
+set xslpath=%~dp0\..\xsl\highlight-file.xsl
+echo xslpath %xslpath%
+set opath=%CD%\xms-out
+echo opath %opath%
+set fullofpath=%opath%\index.html
+echo fullofpath %fullofpath%
+shift
+set iter=0
+set prev=
+set cmd=
+set current=%1
+:loop
+shift
+if [%1]==[] goto afterloop
+set prev=%current%
+set current=%1
+set /a "iter=%iter%+1"
+set /a "mod=%iter%%%2
+if [%mod%]==[1] set cmd=%cmd% %prev%=%current%
+goto loop
+:afterloop
+echo args:  %cmd%
+java -cp "%SAXON_HOME%\saxon9he.jar" net.sf.saxon.Transform -xsl:%xslpath% -it:main sourcepath=%source% link-names=yes output-path=%opath%%cmd%
+start %fullofpath%
