df41e15bde099974c77a69dda63e7664fbba4b01
pgfearo <pgfearo@gmail.com>
2014-12-08 09:19:28 +0000

    xmlspec.bat improved
    paths not hard-coded
    starts browser to show output file

--- app/shell-cmd/xmlspec.bat
+++ app/shell-cmd/xmlspec.bat
@@ -1,7 +1,16 @@
 @echo off
 REM script for calling xmlspectrum with command line such as: xmlspec sample.xml auto-trim=yes indent=2
 REM loop is required to combine name/value pairs with an '=' as args are split as they are passed to CMD
-set source=%1
+set source=%~f1
+echo source: %source%
+set xslpath=%~dp0\..\xsl\highlight-file.xsl
+echo xslpath %xslpath%
+set opath=%CD%\xms-out
+echo opath %opath%
+set ofpath=%~nx1.html
+echo ofpath %ofpath%
+set fullofpath=%opath%\%ofpath%
+echo fullofpath %fullofpath%
 shift
 set iter=0
 set prev=
@@ -17,6 +26,6 @@ set /a "mod=%iter%%%2
 if [%mod%]==[1] set cmd=%cmd% %prev%=%current%
 goto loop
 :afterloop
-echo source: %source%
 echo args:  %cmd%
-java -cp "C:\Saxon\saxon9he.jar" net.sf.saxon.Transform -xsl:C:\Users\Philip\Documents\GitHub\xmlspectrum\app\xsl\highlight-file.xsl -it:main sourcepath=%CD%\%source% output-path=c:\temp\output%cmd%
+java -cp "%SAXON_HOME%\saxon9he.jar" net.sf.saxon.Transform -xsl:%xslpath% -it:main sourcepath=%source% output-path=%opath%%cmd%
+start %fullofpath%
